883d0d5ef797ab88105723cf7dc5fa553fbda0ad
Daria <kurletova@gmail.com>
2026-01-20 20:46:23 +0300

    comments added

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,55 +1,70 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request, Put } from '@nestjs/common';
-import { CommentsService } from './comments.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request,
+  Put,
+} from "@nestjs/common";
+import { CommentsService } from "./comments.service";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { UpdateCommentDto } from "./dto/update-comment.dto";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
 // src/comments/comments.controller.ts
 
+@Controller("post")
 
-
-@Controller('posts/:postId/comments')
-@UseGuards(JwtAuthGuard)
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
-  @Post()
+  @Post(":postId/comments")
+  @UseGuards(JwtAuthGuard)
   async create(
-    @Param('postId') postId: string,
+    @Param("postId") postId: string,
     @Body() createCommentDto: CreateCommentDto,
     @Request() req,
   ) {
-    return this.commentsService.create(req.user.userId, postId, createCommentDto);
+    return this.commentsService.create(
+      req.user.userId,
+      postId,
+      createCommentDto,
+    );
   }
 
-  @Get()
-  async findAllByPost(@Param('postId') postId: string) {
+  @Get(":postId/comments")
+  async findAllByPost(@Param("postId") postId: string) {
     return this.commentsService.findAllByPost(postId);
   }
 
-  @Get(':commentId')
+  @Get(":commentId")
   async findOne(
-    @Param('postId') postId: string,
-    @Param('commentId') commentId: string,
+    @Param("postId") postId: string,
+    @Param("commentId") commentId: string,
   ) {
     return this.commentsService.findOne(commentId);
   }
 
-  @Put(':commentId')
+  @Put(":commentId")
   async update(
-    @Param('commentId') commentId: string,
+    @Param("commentId") commentId: string,
     @Body() updateCommentDto: UpdateCommentDto,
     @Request() req,
   ) {
-    return this.commentsService.update(commentId, req.user.userId, updateCommentDto);
+    return this.commentsService.update(
+      commentId,
+      req.user.userId,
+      updateCommentDto,
+    );
   }
 
-  @Delete(':commentId')
-  async remove(
-    @Param('commentId') commentId: string,
-    @Request() req,
-  ) {
+  @Delete("comments/:commentId")
+  @UseGuards(JwtAuthGuard)
+  async remove(@Param("commentId") commentId: string, @Request() req) {
     return this.commentsService.remove(commentId, req.user.userId);
   }
 }
